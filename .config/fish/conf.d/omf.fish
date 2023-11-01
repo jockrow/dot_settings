d8383ac5891f5bfc24a429113192b64fbc580960
Richard Martínez <r.martinezdk@gmail.com>
2023-11-01 09:43:24 -0300

    FZF:Add Created Column when list with ll and la, Modify Size for Command History

--- .config/fish/conf.d/omf.fish
+++ .config/fish/conf.d/omf.fish
@@ -4,7 +4,12 @@ and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
 or set -gx OMF_PATH "$HOME/.local/share/omf"
 and set -gx OMF_PATH ~/.config/omf/
 
-# Preview file content using bat (https://github.com/sharkdp/bat)
+# List command history
+set -gx FZF_CTRL_R_OPTS "
+	--prompt='󰚩 '
+	--height 80%"
+
+# List file content using bat (https://github.com/sharkdp/bat)
 set -gx FZF_CTRL_T_OPTS "
   --preview 'bat -n --color=always {}'
 	--prompt='󰚩 '
@@ -12,15 +17,16 @@ set -gx FZF_CTRL_T_OPTS "
 
 # TODO:ignorar binarios, probar con /Users/richard/devs/java/api_rest/build, y al verificar que funciona poner que ignore las carpetas build y obj
 # TODO:ignorar node_modules con tree y tre
-
 set -gx FZF_CTRL_T_COMMAND "find . -type f | grep -Ev '\.git|node_modules'"
+
+# List directories
 set -gx FZF_ALT_C_OPTS "--preview 'tree -C {}'
 	--prompt='󰚩 '
 	--height 80%"
 
 alias tre='eza -T --git --icons'
-alias ll='eza --long --header --git --icons'
-alias la='eza --all --long --header --git --icons'
+alias ll='eza --long --created --modified --header --git --icons'
+alias la='eza --all --long --created --modified --header --git --icons'
 alias tree='tree -I \'node_modules | cache |test_*\''
 
 # Load Oh My Fish configuration.
